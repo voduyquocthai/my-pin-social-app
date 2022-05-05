19c384a01cf310bfc97a2ecaa7b59c2590799c5a
voduyquocthai <voduyquocthai@gmail.com>
2022-05-05 17:32:31 +0700

    Added Google OAuth Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './containers/Home';
 
 const App = () => {
-    return <div>My App</div>;
+    return (
+        <Routes>
+            <Route path='login' element={<Login />} />
+            <Route path='/*' element={<Home />} />
+        </Routes>
+    );
 };
 
 export default App;
