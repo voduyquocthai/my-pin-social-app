2d1f2fca93e5e1e62c80f2131a43afc5896e29ae
voduyquocthai <voduyquocthai@gmail.com>
2022-05-06 14:27:22 +0700

    Add Sidebar UI

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,12 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: '2022-05-05',
-  useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2022-05-05',
+    useCdn: true,
+    token: process.env.REACT_APP_SANITY_TOKEN,
+    ignoreBrowserTokenWarning: true,
 });
 
 const builder = imageUrlBuilder(client);
--- frontend/src/containers/Pins.jsx
+++ frontend/src/containers/Pins.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
 
 const Pins = () => {
-  return (
-    <div>Pins</div>
-  )
-}
+    return <div>Pins</div>;
+};
 
-export default Pins
+export default Pins;
